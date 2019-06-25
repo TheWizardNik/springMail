a707f89a573c35f049cef90d664565c21e83b4d8
NikitaSpiridonov <nik-08-99@yandex.ru>
2019-06-25 11:38:14 +0400

    Initial commit

---
+++ src/main/java/com/example/demomail/HtmlEmailExampleController.java
@@ -0,0 +1,44 @@
+package com.example.demomail;
+
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HtmlEmailExampleController {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+
+    @GetMapping("/sendHtmlEmail")
+    public String sendHtmlEmail() throws MessagingException {
+
+        MimeMessage message = emailSender.createMimeMessage();
+
+        boolean multipart = true;
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, multipart, "utf-8");
+
+        String htmlMsg = "<h3>Im testing send a HTML email</h3>"
+                + "<img src='http://www.apache.org/images/asf_logo_wide.gif'>";
+
+        message.setContent(htmlMsg, "text/html");
+
+        helper.setTo(MyConstants.FRIEND_EMAIL);
+
+        helper.setSubject("Test send HTML email");
+
+
+        this.emailSender.send(message);
+
+        return "emailSent";
+    }
+
+}
---
+++ src/main/java/com/example/demomail/MyConstants.java
@@ -0,0 +1,7 @@
+package com.example.demomail;
+
+public class MyConstants {
+    public static final String MY_EMAIL = "example@gmail.com";
+    public static final String MY_PASSWORD = "password";
+    public static final String FRIEND_EMAIL = "example@yandex.ru";
+}
